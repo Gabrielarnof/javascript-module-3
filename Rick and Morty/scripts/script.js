3808e99e7bb2715acf619f1a0228da801f4d30ba
Gabriel Arno <agafarno@gmail.com>
2022-11-04 01:24:12 +0100

    Update script.js

--- Rick and Morty/scripts/script.js
+++ Rick and Morty/scripts/script.js
@@ -1,11 +1,20 @@
-const headerEl = document.createElement("header");
 const mainAreaEl = document.createElement("main");
 const sidebarEl = document.createElement("aside");
 
 // header
-const headerTitleEl = document.createElement("h1");
-headerTitleEl.innerText = "Rick and Morty API";
-headerEl.append(headerTitleEl);
+const createHeader = () => {
+  const headerEl = document.createElement("header");
+  const headerTitleEl = document.createElement("h1");
+  headerTitleEl.innerText = "Rick and Morty API";
+  headerEl.append(headerTitleEl);
+  bodyEl.append(headerEl);
+};
 
+// Aside
+
+cosnt  episodeListEl = document.createElement("ul");
+
+// body
 const bodyEl = document.querySelector("body");
-bodyEl.append(headerEl, sidebarEl, mainAreaEl);
+bodyEl.append(sidebarEl, mainAreaEl);
+createHeader();
